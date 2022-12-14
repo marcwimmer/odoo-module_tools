8b61a4870eab96ca0cf227dc16e1538168c291a8
Marc Wimmer <marc@itewimmer.de>
2022-12-14 10:11:19 +0100

    dependency arrow

--- module_tools/__manifest__.py
+++ module_tools/__manifest__.py
@@ -1,12 +1,14 @@
 {
-    "name" : "module_tools", #usage energy report
+    "name": "module_tools",  # usage energy report
     "description": "Nice tools for all modules",
-    "version" : "13.0.1.0",
-    "author" : "Marc Wimmer",
-    "category" : "Tools",
-    "depends" : ["base"],
-    "update_xml" : [
-                   ],
+    "version": "13.0.1.0",
+    "author": "Marc Wimmer",
+    "category": "Tools",
+    "depends": ["base"],
+    "external_dependencies": {
+        "python": ["arrow"],
+    },
+    "update_xml": [],
     "installable": True,
-    "active": False
+    "active": False,
 }
