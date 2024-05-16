8c5af53e3bea1dc5de26977abcb43c9bb5210b10
Marc Wimmer <marc@zebroo.de>
2024-05-16 22:48:35 +0200

    version in manifest to 1.0

--- module_tools/__manifest__.py
+++ module_tools/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "module_tools",  # usage energy report
     "description": "Nice tools for all modules",
-    "version": "13.0.1.0",
+    "version": "1.0",
     "author": "Marc Wimmer",
     "category": "Tools",
     "depends": ["base"],
