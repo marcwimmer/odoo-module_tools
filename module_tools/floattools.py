02a8043d67ebb7a4476cc65019148f16fd92c61d
Marc Wimmer <marc@zebroo.de>
2024-06-05 08:13:29 +0200

    14.0: Fixed

--- module_tools/floattools.py
+++ module_tools/floattools.py
@@ -5,6 +5,8 @@ def float_is_same(f1, f2):
     s = [str(f1), str(f2)]
 
     def get_len(s):
+        if '.' not in s:
+            s += '.0'
         return len(str(s).split(".")[1])
 
     def round_to_len(f):
